3071cf43d5e9ccd7785d5cdf2f2e1d25efbe76cf
katlyzt <postbotox@gmail.com>
2025-02-04 13:03:47 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ui_build_hw/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ui_build_hw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+
+/*void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+          child: Scaffold(
+        backgroundColor: const Color(0xFFf80602),
+        body: Stack(
+          children: [
+            whiteContainer(),
+            threeAvatars(),
+            blueContainer(),
+            smallContainer(),
+            blueContainerSecond(),
+            smallContainerSecond(),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+Widget whiteContainer() {
+  return Container(
+    height: double.infinity,
+    width: double.infinity,
+    margin: EdgeInsets.all(15),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(25),
+      border: Border.all(color: Colors.black, width: 6),
+    ),
+  );
+}
+
+Widget threeAvatars() {
+  return Padding(
+    padding: const EdgeInsets.only(
+      top: 50,
+      left: 40,
+      right: 40,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CircleAvatar(
+          backgroundColor: const Color(0xFFdd7979),
+          radius: 45,
+        ),
+        CircleAvatar(
+          backgroundColor: const Color(0xFFdd7979),
+          radius: 45,
+        ),
+        CircleAvatar(
+          backgroundColor: const Color(0xFFdd7979),
+          radius: 45,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget blueContainer() {
+  return Align(
+    alignment: Alignment.center,
+    child: Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF6bd0be),
+      ),
+      margin: EdgeInsets.only(bottom: 180),
+      height: 270,
+      width: 270,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 190),
+        child: Align(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 13),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 13),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 13),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 13),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget smallContainer() {
+  return Center(
+    child: Container(
+      decoration: BoxDecoration(
+          color: const Color(0xFF6bacd2),
+          borderRadius: BorderRadius.circular(10)),
+      margin: EdgeInsets.only(bottom: 120),
+      width: 239,
+      height: 182,
+    ),
+  );
+}
+
+Widget blueContainerSecond() {
+  return Align(
+    alignment: Alignment.center,
+    child: Container(
+      margin: EdgeInsets.only(top: 420),
+      height: 270,
+      width: 270,
+      color: const Color(0xFF6bd0be),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 197),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 13),
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 13),
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 13),
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 13),
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: const Color(0xFF6bacd2),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget smallContainerSecond() {*/
+  return Center(
+      child: Container(
+    decoration: BoxDecoration(
+        color: const Color(0xFF6bacd2),
+        borderRadius: BorderRadius.circular(10)),
+    margin: EdgeInsets.only(top: 414.5, left: 55),
+    width: 182,
+    height: 239,
+  ));
+}
+
+
+
+
+
